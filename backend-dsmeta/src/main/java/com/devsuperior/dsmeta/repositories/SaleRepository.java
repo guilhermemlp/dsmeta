8e2bc6d03cb1bbad67e0eba6001e0f558a87534c
Guilherme Magno <guilherme.mprata@gmail.com>
2022-09-05 11:42:48 -0300

    date select

--- backend-dsmeta/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend-dsmeta/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,7 +1,15 @@
 package com.devsuperior.dsmeta.repositories;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("select obj from sale obj where obj.date between :min and :max order by obj.amount desc")
+    Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
